fa2257c9167582b5cc79cc23556344fe15e85297
Jeremy Oglesby <JMO@MacBook-Pro.local>
2019-07-10 13:13:29 -0700

    Adds show page for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,6 +5,10 @@ class PlacesController < ApplicationController
     @places = Place.all.order(:name).page params[:page]
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
   def new
     @place = Place.new
   end
