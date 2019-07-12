6e003024b04770c7c3f3cec1dfacc3c6a57f0fd0
Jeremy Oglesby <JMO@MacBook-Pro.local>
2019-07-11 23:51:52 -0700

    Adds geocoder gem and functionality

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,9 @@ class Place < ApplicationRecord
   paginates_per 5
   # associations
   belongs_to :user
+  # geocoding
+  geocoded_by :address
+  after_validation :geocode
   # validations
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, :description, presence: true
