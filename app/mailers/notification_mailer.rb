811fadcf6df8d46c3035f1e828047613f28c1845
Jeremy Oglesby <iaminaband@gmail.com>
2019-07-24 19:49:28 -0700

    Adds personalization to emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "iaminaband@gmail.com",
-         subject: "Somebody has left a comment on your place!")
+  def comment_added comment
+    @place = comment.place
+    @user = @place.user
+    mail(to: "#{@user.email}",
+         subject: "Somebody has left a comment on #{@place.name}!")
   end
 end
