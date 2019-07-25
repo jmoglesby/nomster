6cefdefdbc56e1f9d299dad5119b2d55903ae724
Jeremy Oglesby <iaminaband@gmail.com>
2019-07-25 00:55:21 -0700

    Adds plumbing for User#show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def show
+    # @user = User.find(user_params[:id])
+  end
+
+  # private #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  # def user_params
+  #   require(User).permit()
+  # end
+end
